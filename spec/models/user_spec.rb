6a28a19f88d4005cdabe453a3be1f5f252d4b054
helman101 <helmanandresortegon@gmail.com>
2021-08-28 11:28:25 +0200

    test User validations and associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:password_confirmation) }
+  it { should validate_uniqueness_of(:email) }
+  it { should allow_value('test@test.com').for(:email) }
+  it { should_not allow_value('test').for(:email) }
+  it { should have_secure_password }
 end
