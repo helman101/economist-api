f53e68c19887c5abc872f60d7c1f5014aee78a6c
helman101 <helmanandresortegon@gmail.com>
2021-08-28 12:22:18 +0200

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_085822) do
+ActiveRecord::Schema.define(version: 2021_08_28_102007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "img_url"
+    t.string "url"
+    t.integer "column_id"
+    t.integer "priority"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
