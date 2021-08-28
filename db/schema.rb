f4f06463f6e789c45a6b6292a99d00ecb4f2c38b
helman101 <helmanandresortegon@gmail.com>
2021-08-28 15:24:54 +0200

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_102007) do
+ActiveRecord::Schema.define(version: 2021_08_28_132121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2021_08_28_102007) do
     t.string "url"
     t.integer "column_id"
     t.integer "priority"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "columns", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
