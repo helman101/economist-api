4d05101b5918b7ee3b6267c4caded48a611a4a60
helman101 <helmanandresortegon@gmail.com>
2021-08-29 14:49:23 +0200

    build create and auth User controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      json_response(@user, :created)
+    else
+      json_response({ errors: @user.errors.full_messages })
+    end
+  end
+
+  def auth
+    user = get_user(user_params)
+    if user
+      if user.authenticate(params[:password])
+        json_response({ id: user.id, name: user.name, email: user.email })
+      else json_response({ errors: user.errors.full_messages })
+      end
+    else json_response({ errors: user.errors.full_messages })
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def get_user(u_params)
+    User.find_by(email: u_params[:email])
+  end
 end
