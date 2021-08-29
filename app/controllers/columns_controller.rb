d066817548e7cf1189bfec51fc404d3e5521b61c
helman101 <helmanandresortegon@gmail.com>
2021-08-29 19:04:16 +0200

    build index controller for columns

---
+++ app/controllers/columns_controller.rb
@@ -0,0 +1,21 @@
+class ColumnsController < ApplicationController
+  def index
+    @columns = Column.all
+
+    @data = set_columns_data(@columns);
+    
+    json_response(@data)
+  end
+
+  private
+
+  def set_columns_data(columns)
+    cols = {}
+
+    columns.each do |elem|
+      cols[elem.id.to_s] = { articles: Article.where(["column_id = ?", elem.id]).order(:priority) }
+    end
+
+    cols
+  end
+end
