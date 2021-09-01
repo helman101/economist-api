4fef16aaf00d846bf2ca7ff8fc31d0b6c488e447
helman101 <helmanandresortegon@gmail.com>
2021-09-01 19:24:48 +0200

    refactor url and img_url wrappers

--- app/models/article_spider.rb
+++ app/models/article_spider.rb
@@ -19,8 +19,8 @@ class ArticleSpider < Kimurai::Base
       column.css('div.e1yv2jhn0').each_with_index do |article, ai|
         item = {}
         item[:title] = article.css('h3').text
-        item[:url] = "https://www.economist.com#{article.css('a').attr('href').value}"
-        item[:img_url] = article.css('img').blank? ? nil : article.css('img').attr('src').value
+        item[:url] = article.css('a').attr('href').value.match(/https:/) ? article.css('a').attr('href').value : "https://www.economist.com#{article.css('a').attr('href').value}"
+        item[:img_url] = article.css('img').empty? ? nil : article.css('img').first.attr('src')
         item[:description] = article.css('p.e1smrlcj0').blank? ? nil : article.css('p.e1smrlcj0').text
         item[:priority] = ai + 1
         item[:column_id] = new_c.id
